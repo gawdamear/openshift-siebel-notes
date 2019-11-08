cc2fa9b5f487b0829540be6ba1c10b2ec3c5ca19
Conor <conor.wymberry@bt.com>
2019-11-08 11:45:38 +0000

    Initial commit

---
+++ Controllers/NotesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.Controllers
+{
+    public class NotesController : Controller
+    {
+        [HttpGet("siebel/notes")]
+        public string Get()
+        {
+            return "Recieving!";
+        }
+    }
+}
